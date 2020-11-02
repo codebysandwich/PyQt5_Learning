33dc55e99fbf73d44ec4299c6fa5830123c61f66
sandwich <122079260@qq.com>
2020-11-02 15:38:28 +0800

    update thread plot

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 pyqt5学习仓库
 
 ## PyQt结合matplot绘图
++ 创建mpl-FigureCanvas控件，布局到QWidget中
+    + 点击按钮可以刷新控件绘图
+    + 控件接受数据实现动态绘图
+        1. 使用QTimer实现
+        2. 使用重载线程方式实现（script/timer.py）
+
+
--- UI/pltInWindow.py
+++ UI/pltInWindow.py
@@ -7,11 +7,19 @@
 @Description: 在pyqt中使用matplotlib
 @FilePath: /PyQt5_Learning/UI/pltInWindow.py
 '''
+
+import sys
+sys.path.append('.')
+from script.timer import Timer
+from PyQt5.QtCore import QTimer
 import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
 from PyQt5.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QPushButton, QVBoxLayout, QWidget, QSizePolicy
+
+# sys.path.append(r'.')
+
 
 class MplCanvas(FigureCanvas):
     def __init__(self) -> None:
@@ -26,13 +34,31 @@ class MplCanvas(FigureCanvas):
         
 
 class UI_Mainwindow(QMainWindow):
+    """[summary]
+    主窗体类设计
+    Args:
+        QMainWindow
+    """
     def __init__(self) -> None:
         super(UI_Mainwindow, self).__init__()
         self.widget = QWidget(self)
         self.canvas = MplCanvas()
         self.setCentralWidget(self.widget)
-        self.bt = QPushButton("liner")
-        self.bt.clicked.connect(self.plot)
+        # 随机绘制折线图按钮
+        self.bt = QPushButton("Liner")
+        # 动态绘制按钮
+        self.bt1 = QPushButton("Dynamic")
+        self.bt.clicked.connect(self.bt_pressed)
+        self.bt1.clicked.connect(self.bt1_pressed)
+        self.bt2 = QPushButton("timer")
+        self.bt2.clicked.connect(self.bt2_pressed)
+        self._timer1 = Timer(self.dynamic_sin_plot)
+        self._timer1.setInterval(100)
+        self.sin_l = [0]
+        self.dynamic_l = []
+        self._timer = QTimer(self)
+        self._timer.setInterval(100)
+        self._timer.timeout.connect(self.dynamic_plot)
 
         self.setupUI()
 
@@ -44,22 +70,60 @@ class UI_Mainwindow(QMainWindow):
         # 添加按钮绘制图片
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.bt)
+        button_layout.addWidget(self.bt1)
+        button_layout.addWidget(self.bt2)
         button_layout.addStretch()
         layout.addLayout(button_layout)
 
-        self.setGeometry(300, 400, 800, 600)
+        self.setGeometry(300, 300, 800, 600)
 
-    def plot(self):
+    def bt_pressed(self):
+        self.plot(5*np.random.randn(100)+12)
+
+    def plot(self, x):
         self.canvas.fig.clear()
         ax = self.canvas.fig.add_subplot(111)
-        ax.plot(5*np.random.randn(100)+12)
+        ax.plot(x)
         self.canvas.fig.tight_layout()
         # 重新绘制GUI
         self.canvas.fig.canvas.draw_idle()
 
+    def bt1_pressed(self):
+        if self.bt1.text() == 'Dynamic':
+            self.bt.setEnabled(False)
+            self.bt1.setText('Stop')
+            self._timer.start()
+        elif self.bt1.text() == 'Stop':
+            self._timer.stop()
+            self.bt1.setText('Dynamic')
+            self.bt.setEnabled(True)
+
+    def dynamic_plot(self):
+        self.dynamic_l.append(np.random.random())
+        self.plot(self.dynamic_l)
+
+    def bt2_pressed(self):
+        if self.bt2.text() == 'timer':
+            self.bt.setEnabled(False)
+            self.bt1.setEnabled(False)
+            self.bt2.setText('stop')
+            QApplication.processEvents()
+            # draw sin
+            self._timer1.start()
+        elif self.bt2.text() == 'stop':
+            self.bt.setEnabled(True)
+            self.bt1.setEnabled(True)
+            self.bt2.setText('timer')
+            QApplication.processEvents()
+            self._timer1.stop()
+
+    def dynamic_sin_plot(self):
+        self.plot(np.sin(self.sin_l))
+        self.sin_l.append(self.sin_l[-1] + 0.1)
+
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication([sys.argv])
     mainwindow = UI_Mainwindow()
     mainwindow.show()
-    app.exec_()
+    sys.exit(app.exec_())
